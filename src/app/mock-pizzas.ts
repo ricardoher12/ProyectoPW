89d4bc01fc1df277cc2a23d8cd1e596021d80936
ricardoher12 <ricardo.hernandezr2@hotmail.com>
2018-09-16 10:00:52 -0600

    Se añaden cambios al mock de objetos y al navbar

--- src/app/mock-pizzas.ts
+++ src/app/mock-pizzas.ts
@@ -1,14 +1,14 @@
 import { Pizza } from './pizza';
 
 export const PIZZAS: Pizza[] = [
-  { id: '11', nombre: 'Mr. Nice', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
-  { id: '12', nombre: 'Narco', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
-  { id: '13', nombre: 'Bombasto', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
-  { id: '14', nombre: 'Celeritas', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
-  { id: '15', nombre: 'Magneta', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
-  { id: '16', nombre: 'RubberMan',picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
-  { id: '17', nombre: 'Dynama',picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
-  { id: '18', nombre: 'Dr IQ', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
-  { id: '19', nombre: 'Magma' ,picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
-  { id: '20', nombre: 'Tornado', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'grande', ingredientes: 'Salsa, queso, tomate, cebolla'},
+  { id: '11', nombre: 'Mr. Nice', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'Grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
+  { id: '12', nombre: 'Narco', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'Grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
+  { id: '13', nombre: 'Bombasto', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'Grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
+  { id: '14', nombre: 'Celeritas', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'Grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
+  { id: '15', nombre: 'Magneta', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'Grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
+  { id: '16', nombre: 'RubberMan',picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'Grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
+  { id: '17', nombre: 'Dynama',picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'Grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
+  { id: '18', nombre: 'Dr IQ', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'Grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
+  { id: '19', nombre: 'Magma' ,picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'Grande', ingredientes: 'Salsa, queso, tomate, cebolla' },
+  { id: '20', nombre: 'Tornado', picture: '../../assets/wallpaper.jpg', forma:'Redonda', size:'Grande', ingredientes: 'Salsa, queso, tomate, cebolla'},
 ];
