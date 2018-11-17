de0856549b063291c30d089027c83013cf8d7fdc
ricardoher12 <ricardo.hernandezr2@hotmail.com>
2018-11-16 19:34:52 -0600

    Cambios con mensaje de exito y error al almacenar una pizza

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -22,7 +22,7 @@ pizzaList: { [id:string] : Pizza} = {};
 flag = "";
 modificarFlag : boolean = false;
 mostrarSeparador: boolean = false;
-url :  string = "http://express-app.westus.azurecontainer.io:3000/api/v1/pizzas";
+url :  string = "http://localhost:3000/api/v1/pizzas";
 private axiosClient: AxiosInstance;
 
 
